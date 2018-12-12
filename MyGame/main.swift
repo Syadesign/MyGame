2ab691f8a628e4dbe039a51661fbb6ddb72707c2
Samahir <samahiradi@gmail.com>
2018-12-12 13:34:59 +0100

    Initial Commit

---
+++ MyGame/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  MyGame
+//
+//  Created by samahir adi on 12/12/2018.
+//  Copyright © 2018 Syadesign. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
