350925561b2d138b624ebb251f86cd41741d6fe8
Samahir <samahiradi@gmail.com>
2018-12-26 18:50:36 +0100

    Add the teamName fonction in the class Game, Add instructions in the func composeATeam class Team, Add a function description in the Character class

---
+++ MyGame/Class/Characters.swift
@@ -0,0 +1,9 @@
+//
+//  Characters.swift
+//  MyGame
+//
+//  Created by samahir adi on 14/12/2018.
+//  Copyright © 2018 Syadesign. All rights reserved.
+//
+
+import Foundation
---
+++ MyGame/Class/Enemy.swift
@@ -0,0 +1,9 @@
+//
+//  Enemy.swift
+//  MyGame
+//
+//  Created by samahir adi on 17/12/2018.
+//  Copyright © 2018 Syadesign. All rights reserved.
+//
+
+import Foundation
